029da60da7ac7a869e81c38e6f4f4919e0d9f96e
Josh Smith <joshsmith2@gmail.com>
2015-04-16 08:10:05 +0100

    Right. QUnit tests passing for loaing functions. I've learned a lot about Javascript scope in the past few hours.

--- js/resize.js
+++ js/resize.js
@@ -3,14 +3,26 @@
     script as a seperate element)
  */
 
+//Declare functions in global scope, for testing
 
+var ResizeModule = function($) {
 
-$(document).ready(function(){
+    var test_brunion = "me";
+
     function get_widths(){
         var widths = [];
         $( img ).each(function(){
-           widths[widths.length] = $( this ).width();
+            widths[widths.length] = $( this ).width();
         });
         return widths;
     }
+
+    return {
+        test_brunion: test_brunion,
+        get_widths: get_widths
+    }
+};
+
+$(document).ready(function(){
+
 });
