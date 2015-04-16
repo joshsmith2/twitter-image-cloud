6f43412d958086c40f3942cffe71d1d2318fcf69
Josh Smith <joshsmith2@gmail.com>
2015-04-16 15:09:17 +0100

    Get_max_count working (or rather, the test is)

--- js/resize.js
+++ js/resize.js
@@ -10,8 +10,17 @@ window.ResizeModule = {};
 
 $(function() {
 
-    var get_counts = function(){
+    //Return a tuple containing the highest count, and the id of the element
+    //which has it
+    var get_max_count = function(){
+        var counts = [];
+        $( 'img' ).each(function(){
+            counts[counts.length] = $( this ).siblings('.count').text()
+        });
+        // Find max count
+        return Math.max.apply(Math,counts);
     };
+
 
 
     var get_widths = function(){
@@ -22,7 +31,9 @@ $(function() {
         return widths;
     };
 
-    ResizeModule.get_widths = get_widths
+    // Return functions for use in unit tests
+    ResizeModule.get_widths = get_widths;
+    ResizeModule.get_max_count = get_max_count;
 });
 
 $(document).ready(function(){
