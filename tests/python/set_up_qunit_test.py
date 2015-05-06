557620b3e009f2051d41135f70e53b6f802d4182
josh <joshsmith2@gmail.com>
2015-05-06 23:57:20 +0100

    Failing main menu test

--- tests/python/set_up_qunit_test.py
+++ tests/python/set_up_qunit_test.py
@@ -11,18 +11,28 @@ except ImportError:
 current_dir = os.path.dirname(os.path.abspath(__file__))
 files = os.path.join(current_dir, 'files')
 tests = os.path.dirname(current_dir)
-db_path = os.path.join(files, 'js_test_out.db')
+
 test_csv_in = os.path.join(files, 'twitter_sample.csv')
 
+qunit_db_path = os.path.join(files, 'qunit_test_out.db')
 qunit_test_file = os.path.join(tests, 'js', 'qunit_test.html')
-merge_test_file = os.path.join(tests, 'js', 'merge_tests.html')
-merge_template = 'merge_tests.html'
-
-cloud = main.ImageCloud(test_csv_in, db_path,
+qunit_cloud = main.ImageCloud(test_csv_in, qunit_db_path,
                         url_column_name='media_urls',
                         html_template='qunit_test.html',
                         html_output_file=qunit_test_file)
-cloud.print_images()
-cloud.html_template = 'merge_tests.html'
-cloud.html_output_file = merge_test_file
-cloud.print_images()
+qunit_cloud.write_csv_file_to_database()
+qunit_cloud.print_images()
+
+merge_db_path = os.path.join(files, 'merge_test.db')
+merge_test_file = os.path.join(tests, 'js', 'merge_tests.html')
+merge_template = 'merge_tests.html'
+
+merge_cloud = main.ImageCloud(test_csv_in, merge_db_path,
+                              url_column_name='media_urls',
+                              html_template='merge_tests.html',
+                              html_output_file=merge_test_file)
+merge_cloud.write_csv_file_to_database()
+merge_cloud.print_images()
+
+os.remove(qunit_db_path)
+os.remove(merge_db_path)
