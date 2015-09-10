56cea95518d34655abb0cb944e25f091160c54e6
josh <joshsmith2@gmail.com>
2015-09-10 16:16:17 +0200

    Functional tests passing again

--- tests/python/functional_tests.py
+++ tests/python/functional_tests.py
@@ -2,8 +2,13 @@ from base import *
 
 class FunctionalTest(GeneralTest):
     def test_whole_process(self):
-        main.print_index(self.test_csv_in, self.test_index,
-                         url_media_column='media_urls' )
+        cloud = main.ImageCloud(csv_file=self.test_csv_in,
+                                html_output_file=self.test_index,
+                                url_column_name='media_urls',
+                                db_path='/tmp/db.db')
+        cloud.write_csv_file_to_database()
+        cloud.print_images()
+        # OK this isn't a great test.
         self.assertTrue(os.path.exists(self.test_index))
 
 if __name__ == '__main__':
